c6ef27bb70db2751b36645aa6b54f1e92fc5136f
laarchambault <laarchambault@gmail.com>
2020-05-02 12:25:00 -0400

    autologin & logout done

--- forecast-planner-api/config/routes.rb
+++ forecast-planner-api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post '/signup', to: "users#create"
   post '/login', to: "users#login"
+  get '/autologin', to: "users#autologin"
+  get '/logout', to: "users#logout"
 end

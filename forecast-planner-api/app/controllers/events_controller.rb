7037c4f2edae09a5021d133df0a68d6d80c94c03
arieldavis22 <aadavisbusiness@gmail.com>
2020-05-01 22:20:09 -0400

    base event form

--- forecast-planner-api/app/controllers/events_controller.rb
+++ forecast-planner-api/app/controllers/events_controller.rb
@@ -1,12 +1,29 @@
 require 'rest-client'
 require 'json'
+# ENV["WEATHER_API_KEY"]
 
 class EventsController < ApplicationController
 
   def index
-    # req = RestClient.get('http://api.weatherbit.io/v2.0/forecast/daily', {params: {'key' => ENV["WEATHER_API_KEY"], 'city' => 'Atlanta,GA'}})
+    events = Event.all
   
-    #   render json: req
+      render json: events
+  end
+
+  def create
+    event = Event.create(title: params["title"], 
+      details: params["details"], 
+      location: params["location"], 
+      date: params["date"]
+    )
+
+
+    req = RestClient.get('http://api.weatherbit.io/v2.0/forecast/daily', {params: {'key' => ENV["WEATHER_API_KEY"], 'city' => event.location}})
+
+    render :json => {
+      event: event,
+      request: req
+    }
   end
 
 
--- forecast-planner-api/db/schema.rb
+++ forecast-planner-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_221951) do
+ActiveRecord::Schema.define(version: 2020_05_02_015236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2020_05_01_221951) do
     t.string "details"
     t.string "location"
     t.date "date"
-    t.time "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

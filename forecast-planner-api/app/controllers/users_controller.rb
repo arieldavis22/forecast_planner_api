26ae6ad0cd78b652e9a4d2a217a7d09b4cf3fb4d
arieldavis22 <aadavisbusiness@gmail.com>
2020-05-04 19:48:10 -0400

    friends

--- forecast-planner-api/app/controllers/users_controller.rb
+++ forecast-planner-api/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 #small edit by Laura
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+
+        render json: users
+    end
+
     def create
         user = User.create(name: params[:name], password: params[:password])
         if user
@@ -10,6 +16,25 @@ class UsersController < ApplicationController
             render json: { error: "Unable to create new user. Please log in or sign up" }, status: :unauthorized
         end
         
+    end
+
+    def addfriend 
+        user = User.find_by(name: params[:currentUser])
+        friend = User.find_by(name: params[:friend])
+
+        friend.frienders << user
+
+        render json: user
+    end
+
+    def seefriends
+        user = User.find_by(name: params[:currentUser])
+
+        if user
+            all_friends = user.friendees.map{ |friend| friend.name }
+
+            render json: all_friends
+        end
     end
 
     def getevents
--- forecast-planner-api/config/routes.rb
+++ forecast-planner-api/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :events 
+  resources :events, :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   post '/signup', to: "users#create"
   post '/login', to: "auth#login"
   get '/autologin', to: "auth#autologin"
   post '/logout', to: "auth#logout"
+  post '/addfriend', to: "users#addfriend"
+  post '/seefriends', to: "users#seefriends"
 
   post '/getevents', to: "users#getevents"
 end
